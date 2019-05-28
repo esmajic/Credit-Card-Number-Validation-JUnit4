c3cf1ec8817a8c1b98d23b15e5abfc6b3fc898bf
unknown <esmajic@yahoo.com>
2019-05-28 18:22:45 +0200

    some additional corrections in test class

--- src/CreditCardTest.java
+++ src/CreditCardTest.java
@@ -5,51 +5,46 @@ import org.junit.Test;
 
 public class CreditCardTest {
 	
-	CreditCard validation;
 	
-	@Before
-	public void setUp() {
-		validation = new CreditCard();
-	}
 
 	@Test
 	public void testIsValid() {
-		boolean condition = validation.isValid(5196081888500645L);
-		assertTrue(true);
+		boolean condition = CreditCard.isValid(5196081888500645L);
+		assertTrue(condition);
 	}
 	
 	@Test
 	public void testGetPrefix() {
-		long result = validation.getPrefix(5196081888500645L, 0);
+		long result = CreditCard.getPrefix(5196081888500645L, 0);
 		assertEquals(5196081888500645L, result);;
 	}
 	
 	@Test
 	public void testGetSize() {
-		int result = validation.getSize(5196081888500645L);
+		int result = CreditCard.getSize(5196081888500645L);
 		assertEquals(16, result);
 	}
 	
 	@Test
 	public void testPrefixMatched() {
-		boolean condition = validation.prefixMatched(5196081888500645L, 16);
-		assertTrue(true);
+		boolean condition = CreditCard.prefixMatched(5196081888500645L, 16);
+		assertTrue(condition);
 	}
 	
 	@Test
 	public void testSumOfOddPlace() {
-		int result = validation.sumOfOddPlace(5196081888500645L);
+		int result = CreditCard.sumOfOddPlace(5196081888500645L);
 		assertEquals(16, result);
 	}
 	
 	@Test
 	public void testGetDigit() {
-		int result = validation.getDigit(8);
+		int result = CreditCard.getDigit(8);
 		assertEquals(8, result);
 	}
 	@Test
 	public void testSumOfDoubleEvenPlace() {
-		int result = validation.sumOfDoubleEvenPlace(5196081888500645L);
+		int result = CreditCard.sumOfDoubleEvenPlace(5196081888500645L);
 		assertEquals(28, result);
 	}
 }
