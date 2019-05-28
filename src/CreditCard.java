05e71a86a78d60ba82cf8ef3b6b2fb205693e471
unknown <esmajic@yahoo.com>
2019-05-28 18:10:39 +0200

    created method sumOfDoubleEvenPlace

--- src/CreditCard.java
+++ src/CreditCard.java
@@ -1,6 +1,13 @@
 
 public class CreditCard {
 
-	
-     
+	public static int sumOfDoubleEvenPlace(long number) {
+		int sum = 0;
+		String num = number + "";
+		for (int i = getSize(number) - 2; i >= 0; i -= 2)
+			sum += getDigit(Integer.parseInt(num.charAt(i) + "") * 2);
+
+		return sum;
+	}
+
 }
