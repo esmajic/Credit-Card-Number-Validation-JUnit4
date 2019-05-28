dace7936309bf51e293673efa90ab79cf546579c
unknown <esmajic@yahoo.com>
2019-05-28 18:14:24 +0200

    created method sumOfOddPlace in CreditCard class

--- src/CreditCard.java
+++ src/CreditCard.java
@@ -4,16 +4,28 @@ public class CreditCard {
 	public static int sumOfDoubleEvenPlace(long number) {
 		int sum = 0;
 		String num = number + "";
-		for (int i = getSize(number) - 2; i >= 0; i -= 2)
+		for (int i = getSize(number) - 2; i >= 0; i -= 2) {
 			sum += getDigit(Integer.parseInt(num.charAt(i) + "") * 2);
+		}
 
 		return sum;
 	}
 
 	public static int getDigit(int number) {
-		if (number < 9)
+		if (number < 9) {
 			return number;
-		return number / 10 + number % 10;
+		} else {
+			return number / 10 + number % 10;
+		}
 	}
 
+	public static int sumOfOddPlace(long number) {
+		int sum = 0;
+		String num = number + "";
+		for (int i = getSize(number) - 1; i >= 0; i -= 2) {
+			sum += Integer.parseInt(num.charAt(i) + "");
+		}
+		return sum;
+
+	}
 }
